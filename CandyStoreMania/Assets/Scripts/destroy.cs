b991a83bfdd34cdf66bc71ec6aabb24f34c72f9f
aa3268 <aa3268@github.com>
2015-10-17 20:19:39 -0400

    Added controls
    Added movement, rotation, jump, interact, fire, alt fire. Added
    controller support as well.

---
+++ CandyStoreMania/Assets/Scripts/destroy.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroy : MonoBehaviour {
+
+	// Use this for initialization
+	void OnParticleCollision(GameObject other)
+	{
+		if (other.name.Contains ("Cube")) {
+			Destroy (other);
+		}
+	}
+}
