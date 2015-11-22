ecaf9b4173b1c06908aaacf931db903c682fe016
microlm <laura.mo@gmail.com>
2015-11-22 01:10:11 -0500

    Added recast walls to improve spring camera

--- CandyStoreMania/Assets/Scripts/CameraSpring.cs
+++ CandyStoreMania/Assets/Scripts/CameraSpring.cs
@@ -27,20 +27,23 @@ public class CameraSpring : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 			
+			
 			Physics.Raycast (origin.transform.position, origin.transform.forward, out hit);
 			distanceObject = hit.distance ;
 
-
+		Debug.DrawRay (origin.transform.position, origin.transform.forward, Color.blue);
 
 		if (distanceObject < limit && cam.transform.localPosition.y > pos_y_close && cam.transform.localPosition.z < pos_z_close) {
 
+			Debug.Log ("FORWARD!!");
 			//ratio =((limit - distanceObject)/limit);
-			cam.transform.Translate(Vector3.forward);
+			cam.transform.localPosition = Vector3.Lerp(cam.transform.localPosition, new Vector3(0, pos_y_close, pos_z_close), Time.deltaTime);
 			//cam.transform.position = Vector3.Lerp(cam.transform.position, new Vector3(0, 4, -4), ratio);
 		}
-		if (distanceObject > 3*limit && cam.transform.localPosition.y < pos_y_far && cam.transform.localPosition.z > pos_z_far) {
+		if (distanceObject > 2*limit && cam.transform.localPosition.y < pos_y_far && cam.transform.localPosition.z > pos_z_far) {
+			Debug.Log ("BACKWARD!!");
 			//ratio =((distanceObject - limit)/limit);
-			cam.transform.Translate(Vector3.back);
+			cam.transform.localPosition = Vector3.Lerp(cam.transform.localPosition, new Vector3(0, pos_y_far, pos_z_far), Time.deltaTime);
 			//cam.transform.position = Vector3.Lerp(cam.transform.position, new Vector3(0, 2, -2), ratio);
 		}
 	}
